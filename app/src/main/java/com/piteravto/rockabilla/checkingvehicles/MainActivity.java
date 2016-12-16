5b0cb9a64d2cf107ca091ea9fcd247dd4d12440a
rockabilla <rockabilla@yandex.ru>
2016-12-16 15:03:32 +0300

    start page with editText for Vehicle + 2 button

--- app/src/main/java/com/piteravto/rockabilla/checkingvehicles/MainActivity.java
+++ app/src/main/java/com/piteravto/rockabilla/checkingvehicles/MainActivity.java
@@ -2,6 +2,10 @@ package com.piteravto.rockabilla.checkingvehicles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText myEditText = (EditText) findViewById(R.id.vehicle_id);
+        myEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                return true;
+            }
+        });
+    }
+
+    public void onClickClearButton(View view) {
+        EditText editText = (EditText) findViewById(R.id.vehicle_id);
+
+        editText.getText().clear();
     }
 }
